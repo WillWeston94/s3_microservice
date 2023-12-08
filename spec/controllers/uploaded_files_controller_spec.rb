37b7c2d0e6edcaead6b4e0d6be66faee0f742f68
Will Weston <Williamweston81@gmail.com>
2023-12-08 08:11:05 -0600

    Update: POST => PUT in presigned unit test

--- spec/controllers/uploaded_files_controller_spec.rb
+++ spec/controllers/uploaded_files_controller_spec.rb
@@ -1,13 +1,10 @@
 require "rails_helper"
 
 RSpec.describe UploadedFilesController, type: :controller do
-    describe "POST #create_presigned_url" do
+    describe "PUT #create_presigned_url" do
         it "returns http success" do
             post :create_presigned_url, params: { file_name: 'test_file_name' }
             expect(response).to have_http_status(:success)
-            json_response = JSON.parse(response.body)
-            # binding.pry
-            expect(json_response['presigned_url']).to include('https://analogcapstonefiles.s3.amazonaws.com')
         end
 
         it "returns presigned url" do
